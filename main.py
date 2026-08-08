8d51af09223f8e4bb8fe04290665d994511f6154
vedantmowad <vedantmowad@gmail.com>
2026-08-08 08:55:40 +0530

    Implemented Qr Code Generation Logic initially

---
+++ main.py
@@ -0,0 +1,27 @@
+import qrcode
+import streamlit as st
+import datetime
+
+st.title("🔳QR Code Generator")
+st.write('''This application generates QR codes based on the input 
+            data provided by the user. You can enter any text or URL, 
+            and it will create a QR code image that you can download.''')
+st.write(f"**Current date and time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}**")
+st.subheader("")
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+data = st.text_input("Enter the data to encode in the QR code:")
+name = st.text_input("Enter the name for the QR code image file:")
+
+if st.button("Generate QR Code"):
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"{name}.png")
+    st.image(f"{name}.png", caption="Generated QR Code")
